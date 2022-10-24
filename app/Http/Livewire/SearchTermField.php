0a17cdc2885f7aa11c21fc867e659ded410a2a6b
thienanvo <thienan_vo@live.be>
2022-10-24 13:58:51 +0200

    Initial commit

---
+++ app/Http/Livewire/SearchTermField.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SearchTermField extends Component
+{
+    public function render()
+    {
+        return view('livewire.search-term-field');
+    }
+}
